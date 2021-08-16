7b2ec18e50269c94a3ee7333474bc77cea2c830d
William <wlwleong@gmail.com>
2021-08-16 21:17:04 +0800

    get_next_line.c updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,51 @@
 /*   By: wlow <wlow@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:10:03 by wlow              #+#    #+#             */
-/*   Updated: 2021/08/11 20:59:12 by wlow             ###   ########.fr       */
+/*   Updated: 2021/08/16 21:16:24 by wlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <limits.h>
+#include <stdio.h>
+
+int	get_newline(char *str, size_t *index, size_t str_len);
 
 char	*get_next_line(int fd)
 {
-	char		*ret_string;
-	static char	buffer_to_read[BUFFER_SIZE + 1];
-	ssize_t		read_size;
+	static t_struct	tab;
+	char			*ret_string;
+	size_t			len;
 
-	if (fd <= 0 || fd > 999 || BUFFER_SIZE < 0)
+	if (read(fd, NULL, 0) < 0)
 		return (NULL);
-	ret_string = NULL;
-	read_size = read(fd, buffer_to_read, BUFFER_SIZE);
-	while (read_size > 0)
+	if (tab.end_of_line == 0)
 	{
-		if (!ret_string)
-			ret_string = ft_strdup(buffer_to_read);
-		else
-			ret_string = ft_strjoin(ret_string, buffer_to_read);
-		read_size = read(fd, buffer_to_read, BUFFER_SIZE);
+		if (tab.end_of_file < 0)
+			return (NULL);
+		tab.n_read = read(fd, tab.read_string, BUFFER_SIZE);
+		if (tab.n_read == 0)
+			return (NULL);
+		if (tab.n_read < BUFFER_SIZE)
+			tab.end_of_file = -1;
+		tab.read_i = -1;
 	}
+	tab.start = ++tab.read_i;
+	tab.end_of_line = get_newline(tab.read_string, &tab.read_i, tab.n_read);
+	len = tab.read_i - tab.start + 1;
+	ret_string = (char *) malloc (sizeof(*ret_string) * (len + 1));
+	ft_memmove(ret_string, &tab.read_string[tab.start], len);
+	ret_string[len] = '\0';
 	return (ret_string);
+}
+
+int	get_newline(char *str, size_t *index, size_t str_len)
+{
+	while (*index < str_len)
+	{
+		if (str[*index] == '\n')
+			return (1);
+		*index += 1;
+	}
+	*index -= 1;
+	return (0);
 }
