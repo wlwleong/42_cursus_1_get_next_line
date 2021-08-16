cd57202f5c58fbca29bc697821525b79530da239
William <wlwleong@gmail.com>
2021-08-16 21:17:33 +0800

    get_next_line_utils.c updated

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,80 +6,31 @@
 /*   By: wlow <wlow@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:10:00 by wlow              #+#    #+#             */
-/*   Updated: 2021/08/11 15:44:08 by wlow             ###   ########.fr       */
+/*   Updated: 2021/08/16 20:33:25 by wlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	*ft_memmove(void *dest, const void *src, size_t count)
 {
-	unsigned int	index;
+	size_t	i;
 
-	if (s)
+	if (count <= 0 || !dest || !src)
+		return (dest);
+	if (src > dest)
 	{
-		index = 0;
-		while (s[index] != '\0')
-			write(fd, &s[index++], 1);
+		i = 0;
+		while (i < count)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
 	}
-}
-
-void	ft_bzero(void *string, size_t n)
-{
-	unsigned char	*str;
-	size_t			i;
-
-	str = (unsigned char *)string;
-	i = 0;
-	while (i < n)
-		str[i++] = 0;
-}
-
-char	*ft_strdup(const char *string)
-{
-	char	*s_dup;
-	int		i;
-
-	i = 0;
-	while (string[i])
-		i++;
-	s_dup = (char *) malloc (sizeof(*s_dup) * (i + 1));
-	if (!s_dup)
-		return (NULL);
-	i = 0;
-	while (string[i])
+	else
 	{
-		s_dup[i] = string[i];
-		i++;
+		while (count-- > 0)
+			((unsigned char *)dest)[count] = ((unsigned char *)src)[count];
 	}
-	s_dup[i] = '\0';
-	return (s_dup);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char			*str;
-	unsigned int	len;
-	int				i;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len = 0;
-	while (s1[len])
-		len++;
-	i = 0;
-	while (s2[i++])
-		len++;
-	str = (char *) malloc (sizeof(*str) * (len + 1));
-	if (!str)
-		return (NULL);
-	len = 0;
-	i = 0;
-	while (s1[i])
-		str[len++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		str[len++] = s2[i++];
-	str[len] = '\0';
-	return (str);
+	return (dest);
 }
