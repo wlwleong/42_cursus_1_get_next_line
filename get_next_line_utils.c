13286b18046c4e1c77b7241c9df10b87aab9de81
William <wlwleong@gmail.com>
2021-08-18 01:32:50 +0800

    get_next_line_utils.c updated

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: wlow <wlow@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:10:00 by wlow              #+#    #+#             */
-/*   Updated: 2021/08/16 20:33:25 by wlow             ###   ########.fr       */
+/*   Updated: 2021/08/18 01:10:01 by wlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,53 @@ void	*ft_memmove(void *dest, const void *src, size_t count)
 			((unsigned char *)dest)[count] = ((unsigned char *)src)[count];
 	}
 	return (dest);
+}
+
+char	*ft_strdup(const char *string)
+{
+	char	*s_dup;
+	int		i;
+
+	i = 0;
+	while (string[i])
+		i++;
+	s_dup = (char *) malloc (sizeof(*s_dup) * (i + 1));
+	if (!s_dup)
+		return (NULL);
+	i = 0;
+	while (string[i])
+	{
+		s_dup[i] = string[i];
+		i++;
+	}
+	s_dup[i] = '\0';
+	return (s_dup);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	unsigned int	len;
+	int				i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = 0;
+	while (s1[len])
+		len++;
+	i = 0;
+	while (s2[i++])
+		len++;
+	str = (char *) malloc (sizeof(*str) * (len + 1));
+	if (!str)
+		return (NULL);
+	len = 0;
+	i = 0;
+	while (s1[i])
+		str[len++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[len++] = s2[i++];
+	str[len] = '\0';
+	return (str);
 }
